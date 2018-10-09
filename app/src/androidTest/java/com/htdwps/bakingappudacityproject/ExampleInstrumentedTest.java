6ddbf3496e9c92ee35e531e897935cb230c12e27
Harry Tran <harrytrandesign@gmail.com>
2018-10-08 23:26:04 -0700

    Add 2nd Espresso Test

--- app/src/androidTest/java/com/htdwps/bakingappudacityproject/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/htdwps/bakingappudacityproject/ExampleInstrumentedTest.java
@@ -1,5 +1,6 @@
 package com.htdwps.bakingappudacityproject;
 
+import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -12,6 +13,7 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -20,6 +22,8 @@ import static android.support.test.espresso.matcher.ViewMatchers.withId;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    private static final String RECIPE_ITEM = "Brownies";
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityTest = new ActivityTestRule<>(MainActivity.class);
@@ -30,13 +34,20 @@ public class ExampleInstrumentedTest {
                 .check(matches(isDisplayed()));
     }
 
-//    @Test
-//    public void browniesInRecyclerViewList() {
-//        onView(withId(R.id.rv_recipe_list))
-//                .perform(RecyclerViewActions
-//                .scrollToPosition(1));
-//
-//        onView(withText("Brownies")).check(matches(isDisplayed()));
-//    }
+    @Test
+    public void browniesInRecyclerViewList() {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        onView(withId(R.id.rv_recipe_list))
+                .perform(RecyclerViewActions
+                .scrollToPosition(1));
+
+        onView(withText(RECIPE_ITEM)).check(matches(isDisplayed()));
+
+    }
 
 }
