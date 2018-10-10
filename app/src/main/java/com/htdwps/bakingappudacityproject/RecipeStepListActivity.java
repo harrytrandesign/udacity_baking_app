17dbe17548e5b9168ea9cebfc1b2a40361a8c4bb
Harry Tran <harrytrandesign@gmail.com>
2018-10-09 18:26:16 -0700

    Removed FAB view.

--- app/src/main/java/com/htdwps/bakingappudacityproject/RecipeStepListActivity.java
+++ app/src/main/java/com/htdwps/bakingappudacityproject/RecipeStepListActivity.java
@@ -4,8 +4,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
@@ -18,6 +16,9 @@ import android.widget.TextView;
 import com.htdwps.bakingappudacityproject.dummy.DummyContent;
 
 import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * An activity representing a list of RecipeSteps. This activity
@@ -33,6 +34,11 @@ public class RecipeStepListActivity extends AppCompatActivity {
      * Whether or not the activity is in two-pane mode, i.e. running on a tablet
      * device.
      */
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.recipestep_list)
+    RecyclerView stepsRecyclerView;
+
     private boolean mTwoPane;
 
     @Override
@@ -40,7 +46,8 @@ public class RecipeStepListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipestep_list);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        ButterKnife.bind(this);
+
         setSupportActionBar(toolbar);
         toolbar.setTitle(getTitle());
 
@@ -51,26 +58,10 @@ public class RecipeStepListActivity extends AppCompatActivity {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        mTwoPane = findViewById(R.id.recipestep_detail_container) != null;
 
-        if (findViewById(R.id.recipestep_detail_container) != null) {
-            // The detail container view will be present only in the
-            // large-screen layouts (res/values-w900dp).
-            // If this view is present, then the
-            // activity should be in two-pane mode.
-            mTwoPane = true;
-        }
-
-        View recyclerView = findViewById(R.id.recipestep_list);
-        assert recyclerView != null;
-        setupRecyclerView((RecyclerView) recyclerView);
+        assert stepsRecyclerView != null;
+        setupRecyclerView(stepsRecyclerView);
     }
 
     private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
